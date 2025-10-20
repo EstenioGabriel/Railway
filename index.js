92cd04998e2e03d4de6646df6d595db4ebd63dde
Estenio Gabriel <esteniogabrieldelima@gmail.com>
2025-10-20 16:00:09 -0300

    removendo rotas

--- index.js
+++ index.js
@@ -7,19 +7,6 @@ app.get("/login", function (req,res){
     res.send("<h1>Primeira Rota</h1>")
 })
 
-// Segunda Rota
-app.get("/cadastro", function (req,res){
-    res.send("<h1>Segunda Rota</h1>")
-
-})// Terceira Rota
-app.get("/login/:nome", function (req,res){
-    res.send("<h1>Terceira Rota</h1>")
-})
-
-
-
-
-
 console.log("PORT env: ", process.env.port);
 
 app.listen(PORT, () => {
