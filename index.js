b7232279a8b5b587b2eed4166951edacf5863d53
Estenio Gabriel <esteniogabrieldelima@gmail.com>
2025-10-21 11:43:55 -0300

    Adicionando novas rotas

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get("/login", function (req,res){
     res.send("<h1>Primeira Rota</h1>")
 })
 
+// adicionar mais duas rotas
+
+// Segunda Rota
+app.get("/cadastro", function (req,res){
+    res.send("<h1>Segunda Rota</h1>")
+})
+
+// Terceira Rota com parametro
+app.get("/login/:qualquercoisa", function (req,res){
+    res.send("<h1>Terceira Rota</h1>")
+})
+
 console.log("PORT env: ", process.env.port);
 
 app.listen(PORT, () => {
